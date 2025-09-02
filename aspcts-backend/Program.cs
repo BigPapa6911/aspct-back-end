7c5a6c237c1d8d266c0fd0247a76a6c246e4a607
BigPapa6911 <gustavogomesmaia@gmail.com>
2025-09-02 11:15:58 -0300

    update: - Atualização do Program.cs e aspcts-backend.csproj

--- aspcts-backend/Program.cs
+++ aspcts-backend/Program.cs
@@ -1,15 +1,83 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using aspcts_backend.Data;
+using aspcts_backend.Services.Interfaces;
+using aspcts_backend.Services;
+using aspcts_backend.Repositories.Interface;
+using aspcts_backend.Repositories;
+using aspcts_backend.Models.Configuration;
+using aspcts_backend.Middleware;
+using aspcts_backend.Helpers;
+using aspcts_backend.Data.Seeds;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure JWT settings
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+var jwtSettings = builder.Configuration.GetSection("JwtSettings").Get<JwtSettings>();
+
+// Configure Entity Framework
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Configure JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings?.Issuer,
+            ValidAudience = jwtSettings?.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtSettings?.SecretKey ?? throw new ArgumentNullException("JWT SecretKey")))
+        };
+    });
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        });
+});
+
+// Configure AutoMapper
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
+// Register repositories
+builder.Services.AddScoped<IGenericRepository<object>, GenericRepository<object>>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IChildRepository, ChildRepository>();
+builder.Services.AddScoped<IAssessmentRepository, AssessmentRepository>();
+builder.Services.AddScoped<ISessionRepository, SessionRepository>();
+builder.Services.AddScoped<IInterventionPlanRepository, InterventionPlanRepository>();
+builder.Services.AddScoped<IReportRepository, ReportRepository>();
+builder.Services.AddScoped<ICommunicationRepository, CommunicationRepository>();
+
+// Register services
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IChildService, ChildService>();
+builder.Services.AddScoped<IAssessmentService, AssessmentService>();
+builder.Services.AddScoped<ISessionService, SessionService>();
+builder.Services.AddScoped<IInterventionPlanService, InterventionPlanService>();
+builder.Services.AddScoped<IReportService, ReportService>();
+builder.Services.AddScoped<ICommunicationService, CommunicationService>();
 
 var app = builder.Build();
 
@@ -22,8 +90,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("AllowReactApp");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseMiddleware<JwtMiddleware>();
+
 app.MapControllers();
+
+// Ensure database is created and seeded
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+
+    var seeder = new DataSeeder(context);
+    await seeder.SeedAsync();
+}
 
 app.Run();
